50b0031d8f468f6d7c1734a636f449dd1bb78eac
Srikant A <shrikanth.akup@gmail.com>
2021-02-27 20:41:18 -0500

    Added input feature

--- testing.py
+++ testing.py
@@ -1,16 +1,17 @@
 def factorial(x):
-	if x==1:
-		return x
-	else:
-		return factorial(x-1) * x
+    if x==1:
+        return x
+    else:
+        return factorial(x-1) * x
 
 
 def factorial2(x):
-	y=0
-	while x>1:
-		y *= x
-		x -= 1
-	return y
+    y=0
+    while x>1:
+        y *= x
+        x -= 1
+        return y
 
-print("8 factorial = " + str(factorial(8)))
-print("8 factorial2 = " + str(factorial(8)))
+val=int(input("Enter Value: "))
+print("factorial = " + str(factorial(val)))
+print("factorial2 = " + str(factorial(val)))
